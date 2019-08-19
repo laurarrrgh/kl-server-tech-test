bb54bdccf837f8058de79372a577c4e1fdce1a8f
Laura <laurajadefogarty@gmail.com>
2019-08-19 08:45:45 +0100

    add apiRouter

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const apiRouter = require("./Routers/apiRouter.js");
 
 app.use(express.json());
+
+app.use("/api", apiRouter);
 
 module.exports = { app };
