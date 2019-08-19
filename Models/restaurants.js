0f4e3167238e84c2da576d3262ac65f0e9e1fc9f
Laura <laurajadefogarty@gmail.com>
2019-08-19 10:41:32 +0100

    removes console log

--- Models/restaurants.js
+++ Models/restaurants.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const { readFile } = require("fs");
 
 const fetchAllRestaurants = cb => {
-  //console.log('fetching restaurants)
   fs.readFile("./restaurants.json", (err, restaurants) => {
     const parsedRestaurants = JSON.parse(restaurants);
     cb(null, parsedRestaurants);
