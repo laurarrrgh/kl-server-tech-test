a574045316cc39e125ef7598d22e69e1388877a2
Laura <laurajadefogarty@gmail.com>
2019-08-19 10:38:45 +0100

    package.json changes

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
+app.use((err, req, res, next) => {
+  res.status(404).send({ code: 404, message: "Page Not Found" });
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ code: 500, message: "Server Error!" });
+});
+
 module.exports = { app };
